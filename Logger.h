e6b96324f992d332d4f7328c33f61344fbee75f8
Darmohrai <darmovad.com>
2024-09-19 21:24:51 +0300

    Change func add_log in class Logger for writing logs time

--- Logger.h
+++ Logger.h
@@ -5,10 +5,12 @@
 #include <fstream>
 #include <queue>
 #include <mutex>
+#include <ctime>
 
 class Logger {
 private:
     static std::queue<std::pair<std::string, std::string>> queue_log;
+    static std::queue<std::string> time_log;
     static bool do_log;
     static std::mutex log_mtx;
 public:
@@ -21,8 +23,9 @@ public:
         std::ofstream fout(log_file, std::ios::app);
         while (do_log or !queue_log.empty()) {
             if (!queue_log.empty()) {
-                fout << "Use " << queue_log.front().first << "| in class - " << queue_log.front().second
+                fout << "Time: " << time_log.front() << "; Use " << queue_log.front().first << "| in class - " << queue_log.front().second
                      << std::endl << "-------------------------------------------------------" << std::endl;
+                time_log.pop();
                 queue_log.pop();
             }
         }
@@ -32,6 +35,16 @@ public:
     static void add_log(std::string method, std::string type_class) {
         std::lock_guard<std::mutex> lockGuard(log_mtx);
         queue_log.push(std::pair<std::string, std::string>(method, type_class));
+
+        time_t now = time(0);
+        tm *ltm = localtime(&now);
+        std::string formattedTime = std::to_string(ltm->tm_year + 1900) + "-"
+                                    + std::to_string(1 + ltm->tm_mon) + "-"
+                                    + std::to_string(ltm->tm_mday) + "_"
+                                    + std::to_string(1 + ltm->tm_hour) + ":"
+                                    + std::to_string(1 + ltm->tm_min) + ":"
+                                    + std::to_string(ltm->tm_sec);
+        time_log.push(formattedTime);
     }
 
     static void start_log() { do_log = true; }
--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ void checkCinAnswer(Function &&func);
 
 #include "Logger.h"
 std::queue<std::pair<std::string, std::string>> Logger::queue_log;
+std::queue<std::string> Logger::time_log;
 bool Logger::do_log;
 std::mutex Logger::log_mtx;
 
